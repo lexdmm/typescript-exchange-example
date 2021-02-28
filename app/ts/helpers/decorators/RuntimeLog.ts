d712fa6b711d98934b3ea13d2bac666db4c7b208
lexdmm <lexdmm@users.noreply.github.com>
2021-02-28 18:49:44 -0300

    example use method decorator for performance test

---
+++ app/ts/helpers/decorators/RuntimeLog.ts
@@ -0,0 +1,40 @@
+/**
+ * @description Get Runtime execution.
+ * @param timeUnit time unit can be milleseconds 'ms' or seconds 's'
+ */
+export function RuntimeLog(timeUnit: string = 'ms') {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+
+        /**
+         * @description 
+         * The three points mean that as we do not know how many parameters are passed, we define a parameter rash, so you can accept 1 or n parameters. 
+         * Javascritpt treats them as an array.
+         * @param args 
+         */
+        descriptor.value = function(...args: any[]) {
+
+            let divider = 1;
+            let unit = 'milliseconds'
+            if(timeUnit === 's') {
+                divider = 1000;
+                unit = 'seconds';
+            }
+
+            console.log('-----------------------');
+            console.log(`Method parameters ${propertyKey}: ${JSON.stringify(args)}`);
+            const t1 = performance.now();
+
+            const ret = originalMethod.apply(this, args);            
+
+            const t2 = performance.now();
+            console.log(`${propertyKey} delayed ${(t2 - t1)/divider} ${unit}`);
+            console.log('-----------------------');
+
+            return ret;
+        }
+
+        return descriptor;
+    }
+    
+}
--- app/ts/models/Exchanges.ts
+++ app/ts/models/Exchanges.ts
@@ -1,3 +1,4 @@
+import { RuntimeLog } from "../helpers/decorators/index";
 import { Exchange } from "./Exchange";
 
 // app/js/models/Negociacao.js
@@ -5,10 +6,12 @@ export class Exchanges {
 
     private _exchanges: Exchange[] = [];
 
+    @RuntimeLog('s')
     add(_exchange: Exchange) {
         this._exchanges.push(_exchange);
     }
 
+    @RuntimeLog('s')
     getArray(): Exchange[] {
         return ([] as Exchange[]).concat(this._exchanges);
     }
