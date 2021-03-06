542ac8099f4c4bf2e56bda615e872b9caa636ec6
lexdmm <lexdmm@users.noreply.github.com>
2021-03-06 18:47:31 -0300

    remove submit, change to event click

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,6 +1,6 @@
 import { ExchangeController } from "./controllers/ExchangeController";
 
 const controller = new ExchangeController();
-$('.form').submit(controller.add.bind(controller)); // This bind is for this. the ExchangeController instance will not be lost when fetching the "add" data.
+$('#add-button').click(controller.add.bind(controller)); // This bind is for this. the ExchangeController instance will not be lost when fetching the "add" data.
 $('#import-button').click(controller.importData.bind(controller));
 
